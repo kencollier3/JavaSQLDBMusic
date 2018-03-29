23c7f03a4e9b796288f2f17b7442d10addba51e4
Ken Collier <mckennoncollier@gmail.com>
2018-03-29 12:52:33 -0600

    added the sort order for albums and artists

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import model.Artist;
 import model.DataSource;
+
+import java.util.List;
 
 public class Main {
 
@@ -10,6 +13,22 @@ public class Main {
             System.out.println("Can't open datasource");
             return;
         }
+
+        List<Artist> artists = dataSource.queryArtists(DataSource.ORDER_BY_ASC);
+        if(artists == null) {
+            System.out.println("No artists!");
+            return;
+        }
+        for (Artist artist : artists) {
+            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
+        }
+
+        List<String> albumsForArtist =
+                dataSource.queryAlbumsForArtist("Iron Maiden", DataSource.ORDER_BY_ASC);
+        for(String album : albumsForArtist) {
+            System.out.println(album);
+        }
+
         dataSource.close();
     }
 }
